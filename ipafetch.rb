63e0cc5303891bd97c1c3444a9f6107dad618636
Andrew <drewgarcia23@gmail.com>
2025-04-17 11:24:53 -0500

    Update

--- ipafetch.rb
+++ ipafetch.rb
@@ -1,16 +1,17 @@
 class Ipafetch < Formula
-  include Language::Python::Virtualenv
-
   desc "A tool to fetch IPA files from Apple Configurator"
   homepage "https://github.com/drewg233/ipafetch"
-  url "https://files.pythonhosted.org/packages/source/i/ipafetch/ipafetch-0.1.0.tar.gz"
-  sha256 "" # You'll need to fill this in after uploading to PyPI
+  url "https://github.com/drewg233/ipafetch.git"
+  version "0.1.0"
   license "MIT"
 
   depends_on "python@3.6"
 
   def install
-    virtualenv_install_with_resources
+    # Install the Python script
+    bin.install "ipafetch/ipafetch.py" => "ipafetch"
+    # Make the script executable
+    chmod 0755, bin/"ipafetch"
   end
 
   test do
